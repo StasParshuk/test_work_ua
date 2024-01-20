09a0a229491c7800f4c223982ee33097bac90be2
stanislav_parshuk <stanislav.parshuk@forforce.com>
2024-01-20 14:43:14 +0200

    configure sonata admin models and generate migrations

---
+++ src/Admin/ClicksAdmin.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+final class ClicksAdmin extends AbstractAdmin
+{
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter
+            ->add('id')
+            ->add('CountClicks')
+            ->add('Date')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('id')
+            ->add('CountClicks')
+            ->add('Date')
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ]);
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('id')
+            ->add('CountClicks')
+            ->add('Date')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->add('id')
+            ->add('CountClicks')
+            ->add('Date')
+        ;
+    }
+}
